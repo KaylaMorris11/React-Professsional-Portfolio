9d059d43f3c60800e29d64aee373ee01cdcc23e4
Kayla Morris <moreiskay14@gmail.com>
2021-09-30 16:32:01 -0700

    Added starter code and files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './NavTab';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+      <Navbar/>
+      <h1>App Component</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Header() {
+  const message = 'Hello world! I am a React Component';
+ 
+  return (
+    <div className="container">
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default Hello;
